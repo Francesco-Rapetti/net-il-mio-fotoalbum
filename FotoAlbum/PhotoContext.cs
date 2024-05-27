55adfcdd6be695ace1d406db717750e721042dc5
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-27 19:32:25 +0200

    Add PhotoContext

---
+++ FotoAlbum/PhotoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Composition;
+using FotoAlbum.Models;
+
+namespace FotoAlbum
+{
+    public class PhotoContext : IdentityDbContext<IdentityUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PhotoAlbum;Integrated Security=True;Trust Server Certificate=True");
+        }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
