d0879e92e35a66b6a51f0e388119ffdb5988b6c4
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-27 19:27:05 +0200

    Add Photo model

---
+++ FotoAlbum/Models/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FotoAlbum.Models
+{
+    public class Photo
+    {
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Img { get; set; }
+        public string ImgSrc { get; set; }
+        public bool IsVisible { get; set; }
+        public List<Category> categories { get; set; }
+        public int UserId { get; set; }
+    }
+}
