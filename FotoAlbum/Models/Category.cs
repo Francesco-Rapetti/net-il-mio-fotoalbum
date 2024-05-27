863007428e1eb7aadbcf83d7cb7b2d5ba6a38f80
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-27 19:26:58 +0200

    Add Category model

---
+++ FotoAlbum/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace FotoAlbum.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Photo> Photos { get; set; }
+    }
+}
