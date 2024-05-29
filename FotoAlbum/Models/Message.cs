57ba8005a28f0e1fa65640f80e36f99e76fb552c
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-29 17:23:40 +0200

    Add Message model

---
+++ FotoAlbum/Models/Message.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FotoAlbum.Models
+{
+    public class Message
+    {
+        [Key] public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        public Message()
+        {
+
+        }
+
+        public Message(string email, string text, string userId)
+        {
+            Email = email;
+            Text = text;
+            UserId = userId;
+        }
+    }
+}
