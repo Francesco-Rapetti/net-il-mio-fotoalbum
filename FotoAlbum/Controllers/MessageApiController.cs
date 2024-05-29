f8816e5eecc900b92bbeb143ce9765f2cb1c49b6
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-29 17:22:08 +0200

    Add MessageApiController

---
+++ FotoAlbum/Controllers/MessageApiController.cs
@@ -0,0 +1,57 @@
+﻿using FotoAlbum.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FotoAlbum.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessageApiController : ControllerBase
+    {
+        private readonly PhotoContext _context;
+
+        public MessageApiController(PhotoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<MessageApiController>
+        [HttpGet]
+        public IActionResult GetAllMessages()
+        {
+            return Ok(_context.Messages.ToList());
+        }
+
+        // GET api/<MessageApiController>/5
+        [HttpGet("{id}")]
+        public IActionResult GetMessage(int id)
+        {
+            return Ok(_context.Messages.Find(id));
+        }
+
+        // POST api/<MessageApiController>
+        [HttpPost]
+        public IActionResult InsertMessage([FromBody] Message message)
+        {
+            _context.Messages.Add(new Message
+            {
+                Email = message.Email,
+                Text = message.Text,
+                UserId = message.UserId
+            });
+            _context.SaveChanges();
+
+            return Ok(true);
+        }
+
+        // DELETE api/<MessageApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _context.Messages.Remove(_context.Messages.Find(id));
+            _context.SaveChanges();
+        }
+    }
+}
