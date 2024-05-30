0c2978cbbc5dc2fbc2ea5921fc83236e7b18fc3d
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-30 02:04:20 +0200

    Add validation

--- FotoAlbum/Models/Category.cs
+++ FotoAlbum/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace FotoAlbum.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FotoAlbum.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
         public List<Photo>? Photos { get; set; }
 
