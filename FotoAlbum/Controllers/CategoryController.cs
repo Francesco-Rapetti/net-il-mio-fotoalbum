f9a623b0f9ad29ff18e73c29e7d2222d9a912bc4
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-30 02:03:15 +0200

    Implements Delete action

--- FotoAlbum/Controllers/CategoryController.cs
+++ FotoAlbum/Controllers/CategoryController.cs
@@ -117,36 +117,21 @@ namespace FotoAlbum.Controllers
         }
 
         // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public IActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var category = _context.Categories.Find(id);
             if (category == null)
             {
                 return NotFound();
             }
-
-            return View(category);
-        }
-
-        // POST: Category/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var category = await _context.Categories.FindAsync(id);
-            if (category != null)
-            {
-                _context.Categories.Remove(category);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            _context.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         private bool CategoryExists(int id)
